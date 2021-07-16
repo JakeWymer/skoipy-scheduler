af2854985d7ee4d537c0f1ce9262ad5266bdc88a
Jake Wymer <wymerbaseball14@gmail.com>
2021-07-15 23:33:51 -0500

    handle generator 404 and change colors (#10)

--- src/components/LoadingSpinner/index.tsx
+++ src/components/LoadingSpinner/index.tsx
@@ -1,10 +1,14 @@
 import MoonLoader from "react-spinners/MoonLoader";
 import styles from "./style.module.scss";
 
-const LoadingSpinner = () => {
+type LoadingSpinnerProps = {
+  color?: string;
+};
+
+const LoadingSpinner = (props: LoadingSpinnerProps) => {
   return (
     <div className={styles.wrapper}>
-      <MoonLoader color="#3a405a" loading size={50} />
+      <MoonLoader color={props.color || "#3a405a"} loading size={50} />
     </div>
   );
 };
