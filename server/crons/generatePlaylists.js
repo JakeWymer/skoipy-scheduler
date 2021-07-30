4b8261264326bed09f978ecb5aa3d430809090c9
Jake Wymer <jakeandrewwymer@gmail.com>
2021-07-29 22:17:15 -0500

    add logging to cron

--- server/crons/generatePlaylists.js
+++ server/crons/generatePlaylists.js
@@ -3,6 +3,7 @@ const { SCHEDULE_TYPES, SCHEDULE_DAYS } = require("../consts");
 const { Op } = require("sequelize");
 const { buildPlaylist } = require("../controllers/generators");
 const { fetchTokens, GrantType } = require("../controllers/auth");
+const logger = require("../logger");
 
 const days = [
   SCHEDULE_DAYS.SUNDAY,
@@ -46,6 +47,8 @@ const days = [
     return shouldGenerate;
   });
 
+  logger.success(`${generators.length} queued up for playlist creation.`);
+
   Promise.all(
     generators.map(async (generator) => {
       const user = generator.User;
@@ -54,6 +57,7 @@ const days = [
         generator.User.refresh_token
       );
       user.accessToken = tokens.accessToken;
+      logger.info(`Generating playlist ${generator.name} for user ${user.id}.`);
       await buildPlaylist(generator, user);
     })
   );
